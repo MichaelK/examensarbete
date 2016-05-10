79f06ee0f357832c527b3ea1333a2a688e2957d4
michaelk <michael.kallio@gmail.com>
2016-05-11 01:54:24 +0200

    documentation for lobbyGUI

--- src/main/java/client/LobbyGUI.java
+++ src/main/java/client/LobbyGUI.java
@@ -11,37 +11,44 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
+ * The GUI that shows the users connected to the chat(port).
  * Created by Michael on 2016-05-08.
  */
 public class LobbyGUI {
+    // The main GUI that starts this one.
+    private SecureChatUI secureChatUI;
 
-    SecureChatUI secureChatUI;
+    // The textarea of the lobby.
+    private TextArea lobbyArea;
 
-    TextArea lobbyArea;
-    Button updateLobby;
+    // Button to update lobby.
+    private Button updateLobby;
 
+    // Constructor
     public LobbyGUI(SecureChatUI secureChatUI){
         this.secureChatUI = secureChatUI;
     }
 
+    // Starts this GUI.
     public void start(){
-
+        // Creates the Stage, Scene and sets Title.
         Stage primaryStage = new Stage();
         Scene scene = new Scene(new Group());
         primaryStage.setTitle("Lobby");
 
-        HBox hBox = new HBox();
-        VBox vBox = new VBox();
-
+        // The textarea of the lobby settings.
         lobbyArea = new TextArea();
         lobbyArea.setPromptText("Lobby");
         lobbyArea.getStyleClass().add("textArea");
         lobbyArea.setEditable(false);
 
+        // The button that updates the lobby.
         updateLobby = new Button("Update Lobby");
         updateLobby.setCenterShape(true);
         updateLobby.setDefaultButton(true);
         updateLobby.getStyleClass().add("updateLobbyButton");
+
+        // Handles the update lobby event.
         updateLobby.setOnAction((event) ->{
             if(this.secureChatUI.getClient() != null){
                 clearLobby();
@@ -53,34 +60,41 @@ public class LobbyGUI {
             }
         });
 
+        // Handles the close GUI event.
         primaryStage.setOnCloseRequest(event -> {
             this.secureChatUI.setLobbyGUI(null);
             primaryStage.close();
         });
 
+        // HBox and VBox settings.
+        HBox hBox = new HBox();
+        VBox vBox = new VBox();
         vBox.setSpacing(3);
         vBox.getChildren().addAll(lobbyArea, updateLobby);
         vBox.setCenterShape(true);
-
         hBox.getStyleClass().add("hboxLobby");
         hBox.setSpacing(5);
         hBox.setPadding(new Insets(25, 25, 25, 25));
         hBox.getChildren().addAll(vBox);
         hBox.setCenterShape(true);
 
+        // Adds children to the scene.
         ((Group) scene.getRoot()).getChildren().addAll(hBox);
-
         primaryStage.setScene(scene);
 
+        // Add stylesheet to the scene.
         scene.getStylesheets().add("myCSS.css");
 
+        // Shows primaryStage.
         primaryStage.show();
     }
 
+    // Clears the lobby.
     void clearLobby(){
         lobbyArea.clear();
     }
 
+    // Appends text to the lobby textarea.
     void appendToLobby(String str) {
         Platform.runLater(new Runnable() {
             @Override
